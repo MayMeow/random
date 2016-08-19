2c9f678b4d1849edda34397cc26441fa2d7e4d35
Charlotta Jung <charlotta.jung@gitlab.cafe>
2016-08-19 11:17:08 +0200

    added php doc comments

--- src/XorShift.php
+++ src/XorShift.php
@@ -2,13 +2,18 @@
 
 namespace AsunaY;
 
-class XorShift {
+class XorShift
+{
 
     protected $x;
     protected $y;
     protected $z;
     protected $w;
 
+    /**
+     * XorShift constructor.
+     * @param null $seed Random generator seed
+     */
     public function __construct($seed = null)
     {
         $this->x = 715225739;
@@ -22,16 +27,23 @@ class XorShift {
         }
     }
 
+    /**
+     * Function getTrand
+     * Return random number between max and min
+     * @param int $min Minimum value
+     * @param int $max Maximum value
+     * @return int
+     */
     public function getRand($min = 0, $max = 0x7fffffff)
     {
         $t = ($this->x ^ ($this->x << 11)) & 0x7fffffff;
 
         $this->x = $this->y;
-		$this->y = $this->z;
-		$this->z = $this->w;
+        $this->y = $this->z;
+        $this->z = $this->w;
 
         $this->w = ( $this->w ^ ($this->w >> 19) ^ ( $t ^ ( $t >> 8 )) );
-		
-		return $this->w % ($max - $min + 1) + $min;
+
+        return $this->w % ($max - $min + 1) + $min;
     }
 }
